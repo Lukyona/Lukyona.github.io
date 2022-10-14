be618ca75dc867a9254fe7389cf922980e64f5bd
Lukyona <66992079+Lukyona@users.noreply.github.com>
2022-10-14 18:51:14 +0900

    Update magnific-popup-setting.js

--- assets/js/magnific-popup-setting.js
+++ assets/js/magnific-popup-setting.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
             type: 'image',
             closeOnContentClick: true,
             showCloseBtn: close,
+          items: {
+              src: $(this).attr('src')
+            },
         });
 				
         $(this).parent('p').css('overflow', 'auto');
 				
-        return '<a href="' + $(this).attr('src') + '" style="width:' + $(this).attr('width') +'px; float: left;"><figure> </figure>' + '<figcaption style="text-align: center;" class="caption">' + $(this).attr('alt') + '</figcaption>' + '</a>';
     });
 });
